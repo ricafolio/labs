2565fe1a877e9598b181eeee454185a9e4fb9e1d
Rica Mae O <29043464+ricafolio@users.noreply.github.com>
2024-03-25 23:21:19 +0800

    #19 Add form validation with tests (#21)
    * feat: add form validation with error message
    
    * feat: use unordered list element for tasks
    
    * feat(test): add tests for form validation

--- src/TaskNew.jsx
+++ src/TaskNew.jsx
@@ -2,20 +2,30 @@ import { useState } from "react"
 
 export function TaskNew({ handleClick }) {
   const [newTask, setNewTask] = useState("")
+  const [hasError, setHasError] = useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const isBadInput = /^\s*$/.test(newTask)
+    if (isBadInput) {
+      setHasError(true)
+      return
+    }
+
+    setHasError(false)
+    handleClick(newTask)
+    setNewTask("")
+  }
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        handleClick(newTask)
-        setNewTask("")
-      }}
-    >
+    <form onSubmit={(e) => handleSubmit(e)}>
       <input
         type="text"
         placeholder="Write new task"
         value={newTask}
         onChange={(e) => setNewTask(e.target.value)}
       />
+      {hasError && <div className="error">Please enter a valid input</div>}
       <button type="submit">Submit</button>
     </form>
   )
