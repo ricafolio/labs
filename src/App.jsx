55bf7beaa3256eb01a3f8c534c61c31c8c496260
ricafolio <29043464+ricafolio@users.noreply.github.com>
2024-04-02 23:02:20 +0800

    feat: add HOC pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+function withStyles(Component, defaultStyle) {
+  return (props) => {
+    const style = {
+      color: "red",
+      fontSize: "1.5em",
+      ...defaultStyle,
+      ...props.style,
+    }
+
+    return <Component {...props} style={style} />
+  }
+}
+
+function Text({ style, customProp }) {
+  return <p style={style}>{customProp ? customProp : "Default text"}</p>
+}
+
+const StyledText = withStyles(Text, { fontWeight: "bold" })
+console.log(StyledText)
+
 export function MyApp() {
   return (
     <>
       <h1>Hello world</h1>
+      <Text />
+      <StyledText />
+      <StyledText customProp="hi" />
+      <StyledText style={{ fontWeight: "regular" }} />
     </>
   )
 }
